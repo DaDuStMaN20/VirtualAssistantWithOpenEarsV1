f4053115a6ce04d13ed5f8f4c7c43b7fe2a9f0a7
Dustin Wasserman <dustin2094@gmail.com>
2016-10-11 12:33:19 -0400

    Squashed a small bug

--- VirtualAssistantWithOpenEarsV1/ContainerViewController.swift
+++ VirtualAssistantWithOpenEarsV1/ContainerViewController.swift
@@ -70,7 +70,7 @@ class ContainerViewController: UIViewController, OEEventsObserverDelegate {
 
             DispatchQueue.main.async {
                 print("This is run on the main queue, after the previous code in outer block")
-                changeToListening()
+                self.changeToListening()
                 
             }
         }
